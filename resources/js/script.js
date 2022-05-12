72f059bd20f9bd138a2df338a50c350457614943
[Justin Topi] <[pvpnet2@gmail.com]>
2022-05-12 03:44:47 +0300

    email service implemented

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,18 @@
-let contact = document.querySelector('.tooltip');
 
-function userNotification() {
-    contact.value = "Coming Soon..."
-    contact.style.color = "red";
-    contact.disabled = true;
+const sendMail = (params) => {
+    let tempParams = {
+        from_name:document.getElementById("fromName").value,
+        message:document.getElementById("msg").value
+    };
+    emailjs.send('service_cs9xtft', 'template_niw74fn', tempParams)
+    .then(function(res){
+        alert("Message sent successfully!")
+    },
+    function(error) {
+        alert("Failed... Please try again later.")
+    }
+    )
 }
 
-contact.addEventListener('click', userNotification);
-
-
-let form = document.getElementById("contactForm");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+let but = document.getElementById('sentButton');
+but.addEventListener('click', sendMail)
